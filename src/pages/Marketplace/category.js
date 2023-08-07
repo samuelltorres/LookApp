79fb678feab82311e12f3d7ff37b5e8f3617e3a2
Samuell <samuelltorres145@gmail.com>
2023-08-07 19:31:41 -0300

    finished drawer navigation and started dynamic header

---
+++ src/pages/Marketplace/category.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Text } from '../../components/index'
+
+export default function Category() {
+  return (
+    <Box justify="center" align="center" background="light">
+      <Text>Product</Text>
+    </Box>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,71 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList
+} from '@react-navigation/drawer'
 import { Home, Feed, SignIn, SignUp } from './pages/index'
+import Icon from 'react-native-vector-icons/SimpleLineIcons'
+
+import { colors } from './styles/theme.json'
+import util from './util.js'
+import { Title } from './components'
 
 const Stack = createStackNavigator()
 const Drawer = createDrawerNavigator()
 
+const CustomDrawerComponent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <Title bold color="light" variant="big" hasPadding>
+        LOOKAPP
+      </Title>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  )
+}
+
 function DrawerComponent() {
   return (
-    <Drawer.Navigator initialRouteName="Feed">
+    <Drawer.Navigator
+      initialRouteName="Feed"
+      drawerContent={(props) => <CustomDrawerComponent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: util.toAlpha(colors.primary, 60),
+        drawerActiveTintColor: colors.light,
+        drawerInactiveTintColor: util.toAlpha(colors.light, 60),
+        drawerStyle: {
+          backgroundColor: colors.midnightblue
+        }
+      }}>
       <Drawer.Screen
+        options={{
+          drawerIcon: ({ color }) => (
+            <Icon name="people" color={color} size={28} />
+          )
+        }}
         name="Feed"
         component={Feed}
-        options={{
-          headerShown: false
-        }}
       />
-      <Drawer.Screen name="Marketplace" component={Feed} />
-      <Drawer.Screen name="Orders" component={Feed} />
+      <Drawer.Screen
+        options={{
+          drawerIcon: ({ color }) => <Icon name="tag" color={color} size={28} />
+        }}
+        name="Marketplace"
+        component={Feed}
+      />
+      <Drawer.Screen
+        options={{
+          drawerIcon: ({ color }) => (
+            <Icon name="basket" color={color} size={28} />
+          )
+        }}
+        name="Orders"
+        component={Feed}
+      />
     </Drawer.Navigator>
   )
 }
